e58211f14af8ae68b8d0344f747d6a31efa2e795
AkilaBV <akilabv@gmail.com>
2021-09-21 14:31:48 -0500

    605_DailyTask_check2wordsAreIsomarphic

---
+++ 605_DailyTask_Check2WordsAreIsomarphic_updated/src/main/java/DailyTask_Check2WordsAreIsomarphic/_DailyTask_Check2WordsAreIsomarphic_updated/App.java
@@ -0,0 +1,13 @@
+package DailyTask_Check2WordsAreIsomarphic._DailyTask_Check2WordsAreIsomarphic_updated;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
